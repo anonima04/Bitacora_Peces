e9b13b3b00a6a50f987dd0bdd47e8dfbb6046fed
Danna <dg5380523@gmail.com>
2024-10-26 23:14:58 -0500

    se ha creado la pagina principal

--- src/Components/Nav/Nav.jsx
+++
@@ -1,9 +0,0 @@
-// import React from 'react'
-import './Nav.css'
-const Nav = () => {
-  return (
-    <div>Hola yo soy tu nuevo nav</div>
-  )
-}
-
-export default Nav
---
+++ src/Page/HomePage/HomePage.jsx
@@ -0,0 +1,18 @@
+// import React from 'react'
+
+import AutoplayCarousels from "../../Components/AutoplayCarousels/AutoplayCarousels"
+import Footer from "../../Components/Footer/Footer"
+import NavBar from "../../Components/NavBar/NavBar"
+
+const HomePage = () => {
+  return (
+   <>
+   <NavBar></NavBar>
+   <AutoplayCarousels></AutoplayCarousels>
+   <Footer></Footer>
+   
+   </>
+  )
+}
+
+export default HomePage
