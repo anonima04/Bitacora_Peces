098889ca9e5bda9270bf5d57ed37f97b2307cee3
SebasGitCourse <ramirezcaperasebas@gmail.com>
2024-10-27 00:27:51 -0500

    creacion login

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
-import { useState } from "react";
+/* eslint-disable no-unused-vars */
+//Importando modulos de FireBase
 import "./App.css";
+import appFireBase from "./Firebase/firebase";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+const auth = getAuth(appFireBase);
+import Login from "./Page/LoginPage/Login";
+import Home from "./Page/HomePage/HomePage";
+import { useState } from "react";
 
 function App() {
-  return <></>;
+  const [usuario, setUsuario] = useState(null);
+
+  onAuthStateChanged(auth, (usuarioFireBase) => {
+    if (usuarioFireBase) {
+      setUsuario(usuarioFireBase);
+    } else {
+      setUsuario(null);
+    }
+  });
+
+  return (
+    <div>{usuario ? <Home correoUsuario={usuario.email} /> : <Login />}</div>
+  );
 }
 
 export default App;
