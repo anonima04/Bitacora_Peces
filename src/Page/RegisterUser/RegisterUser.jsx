c148b696a29db1802f69e6275d129c8c4e064ed3
SebasGitCourse <ramirezcaperasebas@gmail.com>
2024-10-27 18:40:12 -0500

    Desarrollo Registro Usuario

---
+++ src/Page/RegisterUser/RegisterUser.jsx
@@ -0,0 +1,88 @@
+/* eslint-disable no-unused-vars */
+import "./RegisterUser.css";
+import React, { useState } from "react";
+
+const RegisterUser = () => {
+  const [roles] = useState(["Investigador", "Colaborador"]);
+
+  return (
+    <div className="padre">
+      <div className="container-form">
+        <form onSubmit={""} className="formRegister">
+          <span className="textInfo">Cuentanos más sobre ti</span>
+          {/* Nombres */}
+          <div className="divInputNombres">
+            <span className="textNombres">Nombres</span>
+            <input
+              type="text"
+              placeholder="Primer Nombre"
+              className="inputNombre"
+              required
+            />
+            <input
+              type="text"
+              placeholder="Segundo Nombre ( Opcional )"
+              className="inputNombre"
+            />
+          </div>
+
+          {/* Apellidos */}
+          <div className="divInputApellidos">
+            <span className="textApellidos">Apellidos</span>
+            <input
+              type="text"
+              placeholder="Primer Apellido"
+              className="inputApellido"
+              required
+            />
+            <input
+              type="text"
+              placeholder="Segundo Apellido"
+              className="inputApellido"
+              required
+            />
+          </div>
+
+          {/* Nacionalidad */}
+          <div className="divInputTelefono">
+            <span className="textTelefono">Telefono</span>
+            <input
+              type="text"
+              placeholder="Ingresa tu telefono"
+              className="inputTelefono"
+              required
+            />
+          </div>
+
+          {/* Rol */}
+          <div className="divInputRol">
+            <span className="textRol">Rol</span>
+            <select className="selectRol">
+              <option value="">Seleccionar Rol</option>
+              {roles.map((rol, index) => (
+                <option key={index} value={rol} className="option">
+                  {rol}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          {/* Botón de enviar */}
+          <button type="submit" className="btnSubmit">
+            Continuar
+          </button>
+        </form>
+      </div>
+      {/*  */}
+      <div className="floating-div">
+        <img
+          src="https://img.freepik.com/fotos-premium/uma-planta-que-esta-crescendo-na-terra_849715-2537.jpg"
+          alt=""
+          className="floating-img"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default RegisterUser;
