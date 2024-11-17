98d07f777fab4930095ad9e49cceed4691ad4660
SebasGitCourse <ramirezcaperasebas@gmail.com>
2024-11-16 20:31:27 -0500

    index on main: 9c304cd Registro de bitacora

--- src/Components/Authentication/ProtectedRoute.jsx
+++ src/Components/Authentication/ProtectedRoute.jsx
@@ -21,10 +21,10 @@
 
 import { Navigate } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { getAuth } from "firebase/auth";
+import { auth } from "../../Firebase/firebase";
 
 const ProtectedRoute = ({ children, ruta }) => {
-  const [user, loading] = useAuthState(getAuth());
+  const [user, loading] = useAuthState(auth); //Permite verificar el estado de autenticacion de un usuario y obtener el usuario actual en REACT
 
   if (loading) {
     return children; // Se muestra un estado de carga mientras se verifica el usuario
